393d0058cc949a6a845f800d69980c52fd82a6f3
Anatolii <tolikdemchuk507@gmail.com>
2020-09-11 00:45:49 +0300

    good

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ db = SQLAlchemy(app)
 app.secret_key = 'destertopinworld'
 
 
-
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_title = db.Column(db.String(128), nullable=False)
@@ -31,12 +30,31 @@ class Posts(db.Model):
         return '<Posts %r>' % self.id
 
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.Text(), nullable=False)
+    date_reg = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return '<Users %r>' % self.id
+
+
 db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('main.html')
+    if g.user:
+        return render_template('index.html', login=True)
+    else:
+        return render_template('index.html', login=False)
 
 
 #Відображення постів
@@ -47,73 +65,120 @@ def main_page(id_list):
     all_posts = all_posts[id_size:]
     all_posts = all_posts[:10]
     len_posts = len(all_posts)
-    return render_template('all_posts_page.html', all_posts=all_posts, len_posts=len_posts, id_list=id_list)
+    if g.user:
+        return render_template('all_posts_page.html', all_posts=all_posts, len_posts=len_posts, id_list=id_list,
+                               access=True, login=True)
+    return render_template('all_posts_page.html', all_posts=all_posts, len_posts=len_posts, id_list=id_list,
+                           access=False, login=False)
 
 
 #Відображення одного поста
 @app.route('/allPosts/post/<int:id>', methods=['GET', 'POST'])
 def open_post(id):
-    #Перевірка на адміна
-
-    if g.user:
-        #Доступ до видалення і редагування
-        post = Posts.query.get_or_404(id)
-        return render_template('one_post_page.html', post=post, access=True)
     post = Posts.query.get_or_404(id)
-    return render_template('one_post_page.html', post=post, access=False)
-    #Без доступу до видалення і редагування
-
+    if g.user:
+        return render_template('one_post_page.html', post=post, login=True)
+    return render_template('one_post_page.html', post=post, login=False)
 
 
 #Видалення поста
 @app.route('/allPosts/post/<int:id>/delete')
 def delete_post(id):
-    post = Posts.query.get_or_404(id)
-    try:
-        db.session.delete(post)
-        db.session.commit()
-        return redirect('/allPosts/1')
-    except:
-        return "Видалити пост не вийшло"
+    if g.user:
+        post = Posts.query.get_or_404(id)
+        try:
+            db.session.delete(post)
+            db.session.commit()
+            return redirect('/allPosts/1')
+        except:
+            return "Видалити пост не вийшло"
+    return redirect(url_for('index'))
 
 
 #Редагування поста
 @app.route('/allPosts/post/<int:id>/edit', methods=['POST','GET'])
 def edit_post(id):
-    post = Posts.query.get_or_404(id)
-    if request.method == 'POST':
-        post.post_title = request.form['post_title']
-        post.post_text = request.form['post_text']
-        post.post_about = request.form['post_about']
-        post.post_last_edit_time = datetime.datetime.now()
-        try:
-            db.session.commit()
-            return redirect('/allPosts/1')
-        except :
-            return "Не вийшло редагувати пост"
-    else:
-        return render_template("edit_post_page.html", post=post)
+    if g.user:
+        post = Posts.query.get_or_404(id)
+        if request.method == 'POST':
+            post.post_title = request.form['post_title']
+            post.post_text = request.form['post_text']
+            post.post_about = request.form['post_about']
+            post.post_last_edit_time = datetime.datetime.now()
+            try:
+                db.session.commit()
+                return redirect('/allPosts/1')
+            except:
+                return "Не вийшло редагувати пост"
+        else:
+            if g.user:
+                return render_template("edit_post_page.html", post=post, login=True)
+            return render_template("edit_post_page.html", post=post, login=False)
+    return redirect(url_for('index'))
 
 
 #Вхід користувача
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        session.pop('user', None)
+    error_msg = ''
+    if not g.user:
+        if request.method == 'POST':
+            session.pop('user', None)
+            username = request.form['username']
+            acc = Users.query.filter_by(username=username).first()
+            if acc is None:
+                error_msg = 'Такий акаунт не існує'
+                return render_template('login_page.html', error_msg=error_msg, login=False)
+            else:
+                if request.form['password'] == acc.password:
+                    session['user'] = acc.username
+                    return redirect(url_for('index'))
+                else:
+                    error_msg = 'Логін або пароль не правильний'
+                    return redirect(url_for('login', error_msg=error_msg, login=False))
+        return render_template('login_page.html', error_msg=error_msg, login=False)
+    return redirect(url_for('index'))
 
-        if request.form['password'] == 'password':
-            session['user'] = request.form['username']
-        return redirect(url_for('/'))
-    return render_template('login_page.html')
+
+#Реєстрація
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if g.user:
+        return redirect(url_for('index'))
+    else:
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+            check_user = Users.query.filter_by(username=username).first()
+            if check_user is None:
+                acc = Users(username, password, email)
+                try:
+                    db.session.add(acc)
+                    db.session.commit()
+                    return redirect(url_for('index'))
+                except:
+                    return 'Не вийшло створити акаунт'
+            else:
+                return render_template('register_page.html', create=True, login=False)
+        return render_template('register_page.html', create=False, login=False)
 
 
 @app.before_request
 def before_request():
     g.user = None
-
     if 'user' in session:
         g.user = session['user']
 
+
+#drop session
+@app.route('/logout')
+def logout():
+    if g.user:
+        session.pop('user', None)
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
+    
 
 #Створення постів
 @app.route('/create', methods=['POST', 'GET'])
@@ -131,7 +196,9 @@ def create_post():
         except:
             return "Створити пост не вийшло."
     else:
-         return render_template('create_post_page.html')
+        if g.user:
+            return render_template('create_post_page.html', login=True)
+        return render_template('create_post_page.html', login=False)
 
 
 if __name__ == '__main__':
